5282a4790cf1659997c04cd765a84e74667745be
Jonas Daniels <jonas.daniels@outlook.com>
2021-11-07 19:17:31 -0800

    more config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ["@babel/preset-typescript"],
+  presets: [
+    "@babel/preset-typescript",
+    "@babel/preset-react",
+    [
+      "@babel/preset-env",
+      {
+        bugfixes: true,
+        loose: true,
+        targets: "> 1%, not dead, not ie 11, not op_mini all",
+      },
+    ],
+  ],
 };
