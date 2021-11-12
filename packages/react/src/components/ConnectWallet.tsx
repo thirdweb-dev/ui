aa2a4e36e93e214e62b9955b1a0acc1f62e66f67
adam-maj <mr.adam.maj@gmail.com>
2021-11-12 13:40:49 -0500

    Add magic connection to Wallet Connect

--- packages/react/src/components/ConnectWallet.tsx
+++ packages/react/src/components/ConnectWallet.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useMemo } from "react";
 import { 
   Flex,
-  Heading, 
+  Heading,
+  Text, 
   Stack,
   AspectRatio,
   Image,
   Button, 
+  Input,
   useDisclosure,
+  Spinner,
   Modal,
   ModalContent,
   ModalOverlay,
@@ -20,7 +23,9 @@ import { Card } from "./shared/Card";
 
 export const ConnectWallet: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [email, setEmail] = useState<string>("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const { 
     address, 
     provider,
@@ -33,11 +38,19 @@ export const ConnectWallet: React.FC = () => {
     return provider?.provider;
   }, [provider?.provider]);
 
-  function activateConnector(connector: any, options?: any) {
-    if (options) {
-      connectWallet(connector, options);
+  function isEmailValid() {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  async function connectMagic() {
+    if (isEmailValid()) {
+      setEmail("");
+      setLoading(true);
+      await connectWallet("magic", { email })
+      setLoading(false);
     } else {
-      connectWallet(connector);
+      setError(true);
     }
   }
 
@@ -84,6 +97,42 @@ export const ConnectWallet: React.FC = () => {
                   <hr />
                 </>
               )}
+
+              <Stack as={Card} spacing={4}>
+                <Heading as="h4" size="sm" fontWeight="600">
+                  Connect with email
+                </Heading>
+                <Flex direction="column">
+                  <Flex>
+                    <Input 
+                      value={email}
+                      onChange={e => {
+                        setEmail(e.target.value)
+                        setError(false)
+                      }}
+                      placeholder="name@example.com"
+                      borderRadius="4px 0px 0px 4px"
+                    />
+                    <Button 
+                      borderRadius="0px 4px 4px 0px"
+                      width="120px"
+                      onClick={connectMagic}
+                    >
+                      {loading ? (
+                        <Flex>
+                          <Spinner />
+                        </Flex>
+                      ) : "Connect"}
+                    </Button>
+                  </Flex>
+                  {error && (
+                    <Text color="red.400" fontSize="14px" mt="4px"> 
+                      Please enter a valid email.
+                    </Text>
+                  )}
+                </Flex>
+              </Stack>
+
               <Stack as={Card} spacing={4}>
                 <Heading as="h4" size="sm" fontWeight="600">
                   Connect a{address ? " different" : ""} wallet
@@ -98,7 +147,7 @@ export const ConnectWallet: React.FC = () => {
                       <Image src="https://thirdweb.com/logos/metamask-fox.svg" />
                     </AspectRatio>
                   }
-                  onClick={() => activateConnector("injected")}
+                  onClick={() => connectWallet("injected")}
                 >
                   MetaMask
                 </Button>
@@ -113,7 +162,7 @@ export const ConnectWallet: React.FC = () => {
                       <Image src="https://thirdweb.com/logos/walletconnect-logo.svg" />
                     </AspectRatio>
                   }
-                  onClick={() => activateConnector("walletconnect")}
+                  onClick={() => connectWallet("walletconnect")}
                 >
                   WalletConnect
                 </Button>
@@ -128,7 +177,7 @@ export const ConnectWallet: React.FC = () => {
                       <Image src="https://thirdweb.com/logos/coinbase-wallet-logo.svg" />
                     </AspectRatio>
                   }
-                  onClick={() => activateConnector("walletlink")}
+                  onClick={() => connectWallet("walletlink")}
                 >
                   Coinbase Wallet
                 </Button>
