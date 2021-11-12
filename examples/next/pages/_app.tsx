dfb634546e0dd89151580c5f78b301bf1824b8bc
adam-maj <mr.adam.maj@gmail.com>
2021-11-12 14:20:35 -0500

    Add custom connect wallet page

--- examples/next/pages/_app.tsx
+++ examples/next/pages/_app.tsx
@@ -2,25 +2,27 @@ import { ThirdwebWeb3Provider, ThirdwebThemeProvider } from "@3rdweb/react";
 import React from "react";
 
 function ExampleApp({ Component, pageProps }) {
+  const connectors = {
+    injected: {
+      supportedChainIds: [1]
+    },
+    magic: {
+      apiKey: "pk_live_712C1E6230EA31BC",
+      chainId: 1,
+    },
+    walletconnect: {
+      supportedChainIds: [1],
+    },
+    walletlink: {
+      appName: "thirdweb - demo",
+      url: "https://thirdweb.com",
+      darkMode: false,
+      supportedChainIds: [1],
+    }
+  }
+
   return (
-    <ThirdwebWeb3Provider
-      connectors={{
-        injected: { supportedChainIds: [1] },
-        magic: {
-          apiKey: "pk_live_712C1E6230EA31BC",
-          chainId: 1,
-        },
-        walletconnect: {
-          supportedChainIds: [1],
-        },
-        walletlink: {
-          appName: "thirdweb - demo",
-          url: "https://thirdweb.com",
-          darkMode: false,
-          supportedChainIds: [1],
-        },
-      }}
-    >
+    <ThirdwebWeb3Provider connectors={connectors}>
       <ThirdwebThemeProvider> 
         <Component {...pageProps} />
       </ThirdwebThemeProvider>
