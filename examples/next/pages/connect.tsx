7e24cc14e36635cb6a0473c3ab4ddaa21e0bc988
adam-maj <mr.adam.maj@gmail.com>
2021-11-12 14:57:45 -0500

    Add examples home page

---
+++ examples/next/pages/connect.tsx
@@ -0,0 +1,36 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { ConnectWallet } from "@3rdweb/react";
+import React from "react";
+
+export default function ExamplePage() {
+  return (
+    <Flex
+      direction="column"
+      width="100vw"
+      height="100vh"
+      align="center"
+      padding="24px"
+    >
+      <Text mt="64px" fontSize="32px" fontWeight="bold">
+        Web3 Connector
+      </Text>
+      <Text 
+        fontSize="16px"
+        fontWeight="medium" 
+        color="#999" 
+        width="540px"
+        textAlign="center"
+        mb="64px"
+      >
+        Our web3 connector component completely handles wallet connection and network switching and
+        comes with a context wrapper for your app.
+        <br /> <br />
+        You can also customize which connector types you support for your app, including Metamask, Coinbase,
+        WalletConnect, and Magic Link.
+        <br /><br />
+        You can interact with the component below.
+      </Text>
+      <ConnectWallet />
+    </Flex>
+  );
+}
--- examples/next/pages/index.tsx
+++ examples/next/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Heading, Link } from "@chakra-ui/react";
+import { ConnectWallet } from "@3rdweb/react";
+import { Text, Flex, Heading, Link } from "@chakra-ui/react";
 
 export default function() {
   return (
@@ -8,13 +9,16 @@ export default function() {
       align="center"
       justify="center"
     >
-      <Flex direction="column">
+      <Flex direction="column" align="center">
         <Heading
-          margin="0"
+          mt="0px"
+          mb="12px"
           lineHeight="1.15"
+          textAlign="center"
           fontSize="4rem"
         >
           <Link 
+            target="_blank"
             href="https://thirdweb.com"
             color="#0070f3"
             _hover={{
@@ -22,8 +26,39 @@ export default function() {
             }}
           >
             Thirdweb
-          </Link> UI Library
+          </Link> 
+          <br />
+          Component Library
         </Heading>
+
+        <Text 
+          fontSize="16px"
+          fontWeight="medium" 
+          color="#999" 
+          width="540px"
+          textAlign="center"
+        >
+          Welcome to the Thirdweb Component Library. This is a toolbox of all the components
+          you'll need to start easily integrating web3 into your apps.
+          <br /><br />
+          You can view and interact with some of our components below.
+        </Text>
+
+        <Text mt="64px" fontSize="32px" fontWeight="bold">
+          Web3 Connector
+        </Text>
+        <Text 
+          fontSize="16px"
+          fontWeight="medium" 
+          color="#999" 
+          width="540px"
+          textAlign="center"
+          mb="32px"
+        >
+          Our web3 connector completely handles wallet connection and network switching and
+          comes with a context wrapper for your app.
+        </Text>
+        <ConnectWallet />
       </Flex>
     </Flex>
   )
