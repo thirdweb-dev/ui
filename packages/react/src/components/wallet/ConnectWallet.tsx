f7f15c9ff6fc3fb4b38e68efbe60b9495c48a148
adam-maj <mr.adam.maj@gmail.com>
2021-11-17 16:00:17 -0500

    Add modal disconnected

--- packages/react/src/components/wallet/ConnectWallet.tsx
+++ packages/react/src/components/wallet/ConnectWallet.tsx
@@ -2,6 +2,7 @@ import { ButtonProps } from "@chakra-ui/button";
 import {
   AspectRatio,
   Button,
+  Divider,
   Flex,
   Heading,
   Image,
@@ -10,7 +11,6 @@ import {
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalHeader,
   ModalOverlay,
   Spinner,
   Stack,
@@ -19,38 +19,17 @@ import {
 } from "@chakra-ui/react";
 import React, { useMemo, useState } from "react";
 import { useWeb3 } from "../../hooks";
-import { Card } from "../shared/Card";
 import { AddressCopyButton } from "./AddressCopyButton";
 import { ConnectButton } from "./ConnectButton";
+import { ModalDisconnected } from "./ModalDisconnected";
 
 export const ConnectWallet: React.FC<ButtonProps> = ({ ...props }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [email, setEmail] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const { address, provider, connectWallet, disconnectWallet, connectors } =
-    useWeb3();
+  const { address, provider, disconnectWallet } = useWeb3();
 
   const activeProvider = useMemo(() => {
     return provider?.provider;
   }, [provider?.provider]);
-
-  function isEmailValid() {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
-
-  async function connectMagic() {
-    if (isEmailValid()) {
-      setEmail("");
-      setLoading(true);
-      await connectWallet("magic", { email });
-      setLoading(false);
-    } else {
-      setError(true);
-    }
-  }
 
   return (
     <>
@@ -58,140 +37,37 @@ export const ConnectWallet: React.FC<ButtonProps> = ({ ...props }) => {
       <Modal isOpen={isOpen} onClose={onClose} isCentered size="md">
         <ModalOverlay />
         <ModalContent pb={4} bg="gray.50">
-          <ModalHeader as={Heading} size="lg">
-            Connection Manager
-          </ModalHeader>
           <ModalCloseButton />
 
-          <ModalBody>
-            <Stack spacing={4}>
-              {address && (
-                <>
-                  <Stack as={Card}>
-                    <Heading as="h4" size="sm" fontWeight="600">
-                      Connected Wallet
-                    </Heading>
-                    <Flex align="center">
-                      <Flex px={2} direction="column" align="start">
-                        <AddressCopyButton
-                          variant="outline"
-                          address={address}
-                        />
-                      </Flex>
-
-                      <Button
-                        onClick={disconnectWallet}
-                        variant="outline"
-                        ml="auto"
-                        size="sm"
-                      >
-                        {activeProvider?.isMetaMask ? "Switch" : "Disconnect"}
-                      </Button>
-                    </Flex>
-                  </Stack>
-
-                  <hr />
-                </>
-              )}
-
-              {connectors.includes("magic") && (
-                <Stack as={Card} spacing={4}>
+          <ModalBody pt="24px">
+            <Flex direction="column">
+              {address ? (
+                <Stack spacing={4}>
                   <Heading as="h4" size="sm" fontWeight="600">
-                    Connect with email
+                    Connected wallet
                   </Heading>
-                  <Flex direction="column">
-                    <Flex>
-                      <Input
-                        value={email}
-                        onChange={(e) => {
-                          setEmail(e.target.value);
-                          setError(false);
-                        }}
-                        placeholder="name@example.com"
-                        borderRadius="4px 0px 0px 4px"
+                  <Flex align="center">
+                    <Flex direction="column" align="start">
+                      <AddressCopyButton
+                        variant="outline"
+                        address={address}
                       />
-                      <Button
-                        borderRadius="0px 4px 4px 0px"
-                        width="120px"
-                        onClick={connectMagic}
-                      >
-                        {loading ? (
-                          <Flex>
-                            <Spinner />
-                          </Flex>
-                        ) : (
-                          "Connect"
-                        )}
-                      </Button>
                     </Flex>
-                    {error && (
-                      <Text color="red.400" fontSize="14px" mt="4px">
-                        Please enter a valid email.
-                      </Text>
-                    )}
+
+                    <Button
+                      onClick={disconnectWallet}
+                      variant="outline"
+                      ml="auto"
+                      size="sm"
+                    >
+                      {activeProvider?.isMetaMask ? "Switch" : "Disconnect"}
+                    </Button>
                   </Flex>
                 </Stack>
+              ) : (
+                <ModalDisconnected />
               )}
-
-              {connectors.some((connector) => connector !== "magic") && (
-                <Stack as={Card} spacing={4}>
-                  <Heading as="h4" size="sm" fontWeight="600">
-                    Connect a{address ? " different" : ""} wallet
-                  </Heading>
-
-                  {connectors.includes("injected") && (
-                    <Button
-                      size="lg"
-                      variant="outline"
-                      isFullWidth
-                      iconSpacing="auto"
-                      rightIcon={
-                        <AspectRatio ratio={1} w={6}>
-                          <Image src="https://thirdweb.com/logos/metamask-fox.svg" />
-                        </AspectRatio>
-                      }
-                      onClick={() => connectWallet("injected")}
-                    >
-                      MetaMask
-                    </Button>
-                  )}
-
-                  {connectors.includes("walletconnect") && (
-                    <Button
-                      size="lg"
-                      variant="outline"
-                      isFullWidth
-                      iconSpacing="auto"
-                      rightIcon={
-                        <AspectRatio ratio={1} w={6}>
-                          <Image src="https://thirdweb.com/logos/walletconnect-logo.svg" />
-                        </AspectRatio>
-                      }
-                      onClick={() => connectWallet("walletconnect")}
-                    >
-                      WalletConnect
-                    </Button>
-                  )}
-
-                  {connectors.includes("walletlink") && (
-                    <Button
-                      size="lg"
-                      variant="outline"
-                      isFullWidth
-                      iconSpacing="auto"
-                      rightIcon={
-                        <AspectRatio ratio={1} w={6}>
-                          <Image src="https://thirdweb.com/logos/coinbase-wallet-logo.svg" />
-                        </AspectRatio>
-                      }
-                      onClick={() => connectWallet("walletlink")}
-                    >
-                      Coinbase Wallet
-                    </Button>
-                  )}
-                </Stack>
-              )}
-            </Stack>
+            </Flex>
           </ModalBody>
         </ModalContent>
       </Modal>
---
+++ packages/react/src/components/wallet/ModalDisconnected.tsx
@@ -0,0 +1,176 @@
+import React, { useState, useMemo } from "react";
+import { 
+  Flex, 
+  Stack,
+  Heading,
+  Input,
+  Text,
+  Divider,
+  Button,
+  AspectRatio,
+  Image,
+  Spinner
+} from "@chakra-ui/react";
+import { useWeb3 } from "../..";
+import { AddressCopyButton } from "./AddressCopyButton";
+
+export const ModalDisconnected: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const { address, provider, connectWallet, disconnectWallet, connectors } = useWeb3();
+
+  const activeProvider = useMemo(() => {
+    return provider?.provider;
+  }, [provider?.provider]);
+
+  function isEmailValid() {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  async function connectMagic() {
+    if (isEmailValid()) {
+      setEmail("");
+      setLoading(true);
+      await connectWallet("magic", { email });
+      setLoading(false);
+    } else {
+      setError(true);
+    }
+  }
+
+  return (
+    <>
+      <Flex direction="column">
+        {address && (
+          <Stack spacing={4}>
+            <Heading as="h4" size="sm" fontWeight="600">
+              Connected wallet
+            </Heading>
+            <Flex align="center">
+              <Flex direction="column" align="start">
+                <AddressCopyButton
+                  variant="outline"
+                  address={address}
+                />
+              </Flex>
+
+              <Button
+                onClick={disconnectWallet}
+                variant="outline"
+                ml="auto"
+                size="sm"
+              >
+                {activeProvider?.isMetaMask ? "Switch" : "Disconnect"}
+              </Button>
+            </Flex>
+          </Stack>
+        )}
+
+        {connectors.includes("magic") && (
+          <Stack spacing={4}>
+            <Heading as="h4" size="sm" fontWeight="600">
+              Connect with email
+            </Heading>
+            <Flex direction="column">
+              <Flex>
+                <Input
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    setError(false);
+                  }}
+                  placeholder="name@example.com"
+                  borderRadius="4px 0px 0px 4px"
+                />
+                <Button
+                  borderRadius="0px 4px 4px 0px"
+                  width="120px"
+                  onClick={connectMagic}
+                >
+                  {loading ? (
+                    <Flex>
+                      <Spinner />
+                    </Flex>
+                  ) : (
+                    "Connect"
+                  )}
+                </Button>
+              </Flex>
+              {error && (
+                <Text color="red.400" fontSize="14px" mt="4px">
+                  Please enter a valid email.
+                </Text>
+              )}
+            </Flex>
+          </Stack>
+        )}
+
+        {connectors.includes("magic") && connectors.some((connector) => connector !== "magic") && (
+          <Divider mt="32px" mb="24px" width="md" alignSelf="center" />
+        )}
+
+        {connectors.some((connector) => connector !== "magic") && (
+          <Stack spacing={4}>
+            <Heading as="h4" size="sm" mt="0px" fontWeight="600">
+              Connect a{address ? " different" : ""} wallet
+            </Heading>
+
+            {connectors.includes("injected") && (
+              <Button
+                size="lg"
+                variant="outline"
+                isFullWidth
+                iconSpacing="auto"
+                rightIcon={
+                  <AspectRatio ratio={1} w={6}>
+                    <Image src="https://thirdweb.com/logos/metamask-fox.svg" />
+                  </AspectRatio>
+                }
+                onClick={() => connectWallet("injected")}
+              >
+                MetaMask
+              </Button>
+            )}
+
+            {connectors.includes("walletconnect") && (
+              <Button
+                size="lg"
+                variant="outline"
+                isFullWidth
+                iconSpacing="auto"
+                rightIcon={
+                  <AspectRatio ratio={1} w={6}>
+                    <Image src="https://thirdweb.com/logos/walletconnect-logo.svg" />
+                  </AspectRatio>
+                }
+                onClick={() => connectWallet("walletconnect")}
+              >
+                WalletConnect
+              </Button>
+            )}
+
+            {connectors.includes("walletlink") && (
+              <Button
+                size="lg"
+                variant="outline"
+                isFullWidth
+                iconSpacing="auto"
+                rightIcon={
+                  <AspectRatio ratio={1} w={6}>
+                    <Image src="https://thirdweb.com/logos/coinbase-wallet-logo.svg" />
+                  </AspectRatio>
+                }
+                onClick={() => connectWallet("walletlink")}
+              >
+                Coinbase Wallet
+              </Button>
+            )}
+          </Stack>
+        )}
+      </Flex>
+    </>
+  )
+}
