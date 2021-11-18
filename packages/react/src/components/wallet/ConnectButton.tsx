0e8ddc6f9e933fe488e7a9611d8c8cec657d982b
adam-maj <mr.adam.maj@gmail.com>
2021-11-17 21:54:22 -0500

    Add wallet connect button styling

--- packages/react/src/components/wallet/ConnectButton.tsx
+++ packages/react/src/components/wallet/ConnectButton.tsx
@@ -1,14 +1,41 @@
+import React, { useState, useMemo, useEffect } from "react";
 import { Icon } from "@chakra-ui/icons";
-import { Button, Tooltip } from "@chakra-ui/react";
-import React from "react";
+import { formatEther } from "@ethersproject/units";
+import { Button, Stack, Flex, Tooltip, Image, Text, Divider } from "@chakra-ui/react";
 import { IoWalletOutline } from "react-icons/io5";
+import { useSwitchNetwork } from "../..";
 import { useWeb3 } from "../../hooks/useWeb3";
 import { shortenAddress } from "../../utils/shortenAddress";
 
 export const ConnectButton: React.FC<{
   onOpen: () => void;
 }> = ({ onOpen, ...props }) => {
-  const { address } = useWeb3();
+  const { address, provider, chainId } = useWeb3();
+  const { networkMetadata } = useSwitchNetwork();
+  const [renderBalance, setRenderBalance] = useState("");
+
+  useEffect(() => {
+    const getBalance = async () => {
+      if (address) {
+        const balance = await provider?.getBalance(address);
+        setRenderBalance(formatEther(balance || 0).slice(0, 6));
+      } else {
+        setRenderBalance("");
+      }
+    }
+
+    getBalance();
+  }, [provider])
+
+  const renderSymbol = useMemo(() => {
+    return (chainId && networkMetadata[chainId]?.symbol) || "";
+  }, [chainId]);
+
+  const renderIcon = useMemo(() => {
+    return (
+      (chainId && networkMetadata[chainId]?.iconUrl) || undefined
+    );
+  }, [chainId]);
 
   return (
     <Tooltip
@@ -19,16 +46,67 @@ export const ConnectButton: React.FC<{
           : "Connect your wallet to get started"
       }
     >
-      <Button
-        px={6}
-        borderRadius="8px"
-        leftIcon={<Icon mt="-1px" as={IoWalletOutline}></Icon>}
-        onClick={onOpen}
-        iconSpacing={3}
-        {...props}
-      >
-        {address ? shortenAddress(address) : "Connect Wallet"}
-      </Button>
+      {address ? (
+        <Flex 
+          borderRadius="25px"
+          borderWidth="1px"
+          borderColor="gray.300"
+          padding="5px"
+          height="50px"
+          align="center"
+          onClick={onOpen}
+          cursor="pointer"
+          _hover={{
+            borderColor: "#5CC4FF"
+          }}
+          {...props}
+        >
+          <Stack flexShrink={0} direction="row" align="center" pr={3}>
+            {renderIcon && (
+              <Image 
+                height="40px"
+                width="40px"
+                borderRadius="25px"
+                src={renderIcon} 
+              />
+            )}
+            <Stack textAlign="left" justify="flex-start" spacing={0}>
+              <Text size="label.md" color="heading" lineHeight={1}>
+                {shortenAddress(address)}
+              </Text>
+              <Text color="gray.500" fontSize="12px" lineHeight={1}>
+                {chainId && networkMetadata[chainId]?.chainName}
+              </Text>
+            </Stack>
+          </Stack>
+          <Divider
+            borderColor="gray.300"
+            flexShrink={0}
+            orientation="vertical"
+          />
+          <Text 
+            flexShrink={0} px={3} 
+            fontSize="12px" 
+            color="#0098EE" 
+            lineHeight="14px"
+          >
+            {renderBalance}
+            {renderSymbol.length > 2 && <br />}
+            {renderSymbol}
+          </Text>
+        </Flex>
+      ) : (
+        <Button
+          px={6}
+          borderRadius="8px"
+          leftIcon={<Icon mt="-1px" as={IoWalletOutline}></Icon>}
+          onClick={onOpen}
+          iconSpacing={3}
+          {...props}
+        >
+          Connect Wallet
+        </Button>
+      )}
     </Tooltip>
   );
 };
--- packages/react/src/hooks/useSwitchNetwork.ts
+++ packages/react/src/hooks/useSwitchNetwork.ts
@@ -6,27 +6,33 @@ import { useThirdwebContext } from "../components/providers/Web3Provider";
 const defaultNetworkMetadata = {
   1: {
     chainName: "Ethereum Mainnet",
-    iconUrl: "https://ethereum.org/static/4b5288012dc4b32ae7ff21fccac98de1/31987/eth-diamond-black-gray.png"
+    iconUrl: "https://ethereum.org/static/4b5288012dc4b32ae7ff21fccac98de1/31987/eth-diamond-black-gray.png",
+    symbol: "ETH"
   },
   4: {
     chainName: "Rinkeby Testnet",
-    iconUrl: "https://ethereum.org/static/4b5288012dc4b32ae7ff21fccac98de1/31987/eth-diamond-black-gray.png"
+    iconUrl: "https://ethereum.org/static/4b5288012dc4b32ae7ff21fccac98de1/31987/eth-diamond-black-gray.png",
+    symbol: "ETJ"
   }, 
   137: {
     chainName: "Matic Mainnet",
-    iconUrl: "https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png"
+    iconUrl: "https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png",
+    symbol: "MATIC"
   }, 
   250: {
     chainName: "Fantom Opera",
-    iconUrl: "https://icodrops.com/wp-content/uploads/2018/04/teryT6Hw_400x400.jpg"
+    iconUrl: "https://icodrops.com/wp-content/uploads/2018/04/teryT6Hw_400x400.jpg",
+    symbol: "FTM"
   }, 
   43114: {
     chainName: "Avalanche",
-    iconUrl: "https://assets.website-files.com/6059b554e81c705f9dd2dd32/60ec6a944b52e3e96e16af68_Avalanche_Square_Red_Circle.png"
+    iconUrl: "https://assets.website-files.com/6059b554e81c705f9dd2dd32/60ec6a944b52e3e96e16af68_Avalanche_Square_Red_Circle.png",
+    symbol: "AVAX"
   }, 
   80001: {
     chainName: "Matic Mumbai",
-    iconUrl: "https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png"
+    iconUrl: "https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png",
+    symbol: "MATIC"
   }
 }
 
